ee0d6427484a238bb6f8d0fe127bcd5d3ce91960
Fabricio-Guima <fsgkof@gmail.com>
2024-11-21 17:04:32 -0300

    basic routes

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_flutter_app/config/menu/menu_items.dart';
+import 'package:widgets_flutter_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -53,6 +54,13 @@ class _CustomListTile extends StatelessWidget {
        subtitle: Text(menuItem.subTitle),
        onTap: () {
         // navigate to the another screen
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(
+        //     builder: (context) => const ButtonsScreen(),
+        //   ),
+        // );
+
+        Navigator.pushNamed(context, menuItem.link);
        },
     );
   }
