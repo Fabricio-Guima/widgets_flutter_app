5e18a70852edfebcf643879a809955491f2156e5
Fabricio-Guima <fsgkof@gmail.com>
2024-11-24 11:16:19 -0300

    custom button

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -64,6 +64,8 @@ class _ButtonsView extends StatelessWidget {
               label: const Text('Text Icon')
             ),
 
+            const CustomButton(),
+
             IconButton(onPressed: () {}, icon: const Icon(Icons.app_registration_rounded)),
             IconButton(
               onPressed: () {}, 
@@ -76,6 +78,28 @@ class _ButtonsView extends StatelessWidget {
           ],
         )
         ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors  = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child:  Text('Hello world', style: TextStyle(color: Colors.white))
+            ),
+        ),
+      ),
     );
   }
 }
